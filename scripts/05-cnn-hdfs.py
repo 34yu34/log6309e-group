bc6483a8acd086f9f4d6a1ee9ec67507c721d8fd
Simon Chamorro <simon.chamorro@usherbrooke.ca>
2023-10-06 10:41:31 -0400

    replace ae and transformer with cnn and lstm

--- scripts/05-cnn-hdfs.py
+++ scripts/05-cnn-hdfs.py
@@ -5,34 +5,34 @@ import sys
 sys.path.append('../deep-loglizer/')
 import argparse
 from torch.utils.data import DataLoader
-from deeploglizer.models import Transformer
-from deeploglizer.common.preprocess import FeatureExtractor
+
+from deeploglizer.models import CNN
 from deeploglizer.common.dataloader import load_sessions, log_dataset
+from deeploglizer.common.preprocess import FeatureExtractor
 from deeploglizer.common.utils import seed_everything, dump_final_results, dump_params
 
 
 parser = argparse.ArgumentParser()
 
 ##### Model params
-parser.add_argument("--model_name", default="Transformer", type=str)
+parser.add_argument("--model_name", default="CNN", type=str)
 parser.add_argument("--hidden_size", default=128, type=int)
-parser.add_argument("--num_layers", default=2, type=int)
+parser.add_argument("--kernel_sizes", default="2 3 4", nargs="+")
 parser.add_argument("--embedding_dim", default=32, type=int)
-parser.add_argument("--nhead", default=2, type=int)
 
 ##### Dataset params
 parser.add_argument("--dataset", default="HDFS", type=str)
 parser.add_argument(
-    "--data_dir", default="data/HDFS_2k/hdfs_1.0_tar", type=str
+    "--data_dir", default="data/HDFS_v1/hdfs_1.0_tar", type=str
 )
 parser.add_argument("--window_size", default=10, type=int)
 parser.add_argument("--stride", default=1, type=int)
 
 ##### Input params
-parser.add_argument("--feature_type", default="sequentials", type=str)
-parser.add_argument("--use_attention", action="store_true")
-parser.add_argument("--label_type", default="next_log", type=str)
+parser.add_argument("--feature_type", default="sequentials", type=str, choices=["sequentials", "semantics"])
+parser.add_argument("--label_type", default="anomaly", type=str)
 parser.add_argument("--use_tfidf", action="store_true")
+parser.add_argument("--pretrain_path", default=None, type=str)
 parser.add_argument("--max_token_len", default=50, type=int)
 parser.add_argument("--min_token_count", default=1, type=int)
 # Uncomment the following to use pretrained word embeddings. The "embedding_dim" should be set as 300
@@ -44,7 +44,6 @@ parser.add_argument("--min_token_count", default=1, type=int)
 parser.add_argument("--epoches", default=100, type=int)
 parser.add_argument("--batch_size", default=1024, type=int)
 parser.add_argument("--learning_rate", default=0.01, type=float)
-parser.add_argument("--topk", default=10, type=int)
 parser.add_argument("--patience", default=3, type=int)
 
 ##### Others
@@ -58,7 +57,6 @@ model_save_path = dump_params(params)
 
 if __name__ == "__main__":
     seed_everything(params["random_seed"])
-
     session_train, session_test = load_sessions(data_dir=params["data_dir"])
     ext = FeatureExtractor(**params)
 
@@ -75,9 +73,7 @@ if __name__ == "__main__":
         dataset_test, batch_size=4096, shuffle=False, pin_memory=True
     )
 
-    model = Transformer(
-        meta_data=ext.meta_data, model_save_path=model_save_path, **params
-    )
+    model = CNN(meta_data=ext.meta_data, model_save_path=model_save_path, **params)
 
     eval_results = model.fit(
         dataloader_train,
@@ -93,7 +89,6 @@ if __name__ == "__main__":
         "train_anomaly_ratio",
         "feature_type",
         "label_type",
-        "use_attention",
     ]
 
     args_str = "\t".join(
--- scripts/06-lstm-hdfs.py
+++ scripts/06-lstm-hdfs.py
@@ -6,19 +6,20 @@ sys.path.append('../deep-loglizer/')
 import argparse
 from torch.utils.data import DataLoader
 
-from deeploglizer.models import AutoEncoder
+from deeploglizer.models import LSTM
 from deeploglizer.common.preprocess import FeatureExtractor
 from deeploglizer.common.dataloader import load_sessions, log_dataset
-from deeploglizer.common.utils import seed_everything, dump_params, dump_final_results
+from deeploglizer.common.utils import seed_everything, dump_final_results, dump_params
 
 
 parser = argparse.ArgumentParser()
 
 ##### Model params
-parser.add_argument("--model_name", default="Autoencoder", type=str)
+parser.add_argument("--model_name", default="LSTM", type=str)
+parser.add_argument("--use_attention", action="store_true")
 parser.add_argument("--hidden_size", default=128, type=int)
-parser.add_argument("--num_directions", default=2, type=int)
 parser.add_argument("--num_layers", default=2, type=int)
+parser.add_argument("--num_directions", default=2, type=int)
 parser.add_argument("--embedding_dim", default=32, type=int)
 
 ##### Dataset params
@@ -31,6 +32,7 @@ parser.add_argument("--stride", default=1, type=int)
 
 ##### Input params
 parser.add_argument("--feature_type", default="sequentials", type=str, choices=["sequentials", "semantics"])
+parser.add_argument("--label_type", default="next_log", type=str)
 parser.add_argument("--use_tfidf", action="store_true")
 parser.add_argument("--max_token_len", default=50, type=int)
 parser.add_argument("--min_token_count", default=1, type=int)
@@ -43,13 +45,12 @@ parser.add_argument("--min_token_count", default=1, type=int)
 parser.add_argument("--epoches", default=100, type=int)
 parser.add_argument("--batch_size", default=1024, type=int)
 parser.add_argument("--learning_rate", default=0.01, type=float)
-parser.add_argument("--anomaly_ratio", default=0.1, type=float)
+parser.add_argument("--topk", default=10, type=int)
 parser.add_argument("--patience", default=3, type=int)
 
 ##### Others
 parser.add_argument("--random_seed", default=42, type=int)
 parser.add_argument("--gpu", default=0, type=int)
-parser.add_argument("--cache", default=False, type=bool)
 
 params = vars(parser.parse_args())
 
@@ -60,6 +61,7 @@ if __name__ == "__main__":
     seed_everything(params["random_seed"])
 
     session_train, session_test = load_sessions(data_dir=params["data_dir"])
+
     ext = FeatureExtractor(**params)
 
     session_train = ext.fit_transform(session_train)
@@ -75,15 +77,27 @@ if __name__ == "__main__":
         dataset_test, batch_size=4096, shuffle=False, pin_memory=True
     )
 
-    model = AutoEncoder(
-        meta_data=ext.meta_data, model_save_path=model_save_path, **params
-    )
+    model = LSTM(meta_data=ext.meta_data, model_save_path=model_save_path, **params)
 
     eval_results = model.fit(
         dataloader_train,
         test_loader=dataloader_test,
         epoches=params["epoches"],
         learning_rate=params["learning_rate"],
+    )
+
+    result_str = "\t".join(["{}-{:.4f}".format(k, v) for k, v in eval_results.items()])
+
+    key_info = [
+        "dataset",
+        "train_anomaly_ratio",
+        "feature_type",
+        "label_type",
+        "use_attention",
+    ]
+
+    args_str = "\t".join(
+        ["{}:{}".format(k, v) for k, v in params.items() if k in key_info]
     )
 
     dump_final_results(params, eval_results, model)
