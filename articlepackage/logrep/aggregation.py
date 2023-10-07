6a22919b0550a8bfddca6d0b3508891047850f84
nicolegros <nicolaslegros98@gmail.com>
2023-10-07 16:46:48 -0400

    :construction: Working on processing

--- articlepackage/logrep/aggregation.py
+++ articlepackage/logrep/aggregation.py
@@ -1,31 +1,49 @@
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-
-# aggregate log event-level representation to seqence-level
-
-structured_logs = pd.read_csv('../../data/BGL_2k/BGL_2k.log_structured.csv')
-evt_level_rep = pd.DataFrame.to_numpy(structured_logs)
-
-x_train = evt_level_rep["x_train"][()]
-y_train = evt_level_rep["y_train"]
-x_test = evt_level_rep["x_test"][()]
-y_test = evt_level_rep["y_test"]
-
-# Aggregation
-x_train_agg = []
-for i in tqdm(range(x_train.shape[0])):
-    fea = np.mean(x_train[i], axis = 0)
-    x_train_agg.append(fea)
-x_train_agg = np.array(x_train_agg)
-x_train_agg.shape
+from sklearn.model_selection import train_test_split
 
 
-x_test_agg = []
-for i in tqdm(range(x_test.shape[0])):
-    fea = np.mean(x_test[i], axis = 0)
-    x_test_agg.append(fea)
-x_test_agg = np.array(x_test_agg)
-x_test_agg.shape
+def aggregate(input_file_name, output_file_name):
+    # aggregate log event-level representation to sequence-level
 
-np.savez('data/BGL_2k/agg/BGL_word2vec_template_TFIDF_300d_agg.npz',x_train=x_train_agg, y_train = y_train, x_test = x_test_agg, y_test= y_test)
+    df = pd.read_csv(input_file_name + '.csv')
+    evt_level_rep = pd.DataFrame.to_numpy(df)
+
+
+    df['Time'] = pd.to_datetime(df['Time'], format="%Y-%m-%d-%H.%M.%S.%f")
+    g = df.groupby(pd.Grouper(key='Time', freq='6H'))    # x_train = train.to_dict('index')
+
+    train, test = train_test_split(g.mean(), test_size=0.2)
+
+    x_train = train
+    y_train = np.zeros(len(train))
+    # x_test = test.to_dict('index')
+    x_test = test
+    y_test = np.ones(len(test))
+
+    # Aggregation
+    x_train_agg = []
+    # for i in tqdm(range(len(x_train))):
+    for i in tqdm(range(x_train.shape[0])):
+        fea = np.mean(x_train.to_numpy()[i], axis=0)
+        x_train_agg.append(fea)
+    x_train_agg = np.array(x_train_agg)
+    print(x_train_agg.shape)
+
+    x_test_agg = []
+    # for i in tqdm(range(len(x_test))):
+    for i in tqdm(range(x_test.shape[0])):
+        fea = np.mean(x_test[i], axis=0)
+        x_test_agg.append(fea)
+
+    x_test_agg = np.array(x_test_agg)
+    print(x_test_agg.shape)
+
+    np.savez(output_file_name + '.npz',
+             x_train=x_train_agg,
+             y_train=y_train,
+             x_test=x_test_agg,
+             y_test=y_test)
+aggregate('../../data/BGL_2k/BGL_2k.log_structured', '../../data/BGL_2k/BGL_2k.agg')
+# aggregate('../../data/BGL/BGL.log_structured', '../../data/BGL/BGL.agg')
--- src/01-parse_data_bgl.py
+++ src/01-parse_data_bgl.py
@@ -3,31 +3,29 @@
 import sys
 from logparser.Drain import LogParser
 
-# input_dir  = 'data/BGL/' # The input directory of log file
-# output_dir = 'data/BGL'  # The output directory of parsing results
-# log_file   = 'BGL.log'  # The input log file name
-input_dir  = '../data/BGL_2k/' # The input directory of log file
-output_dir = '../data/BGL_2k'  # The output directory of parsing results
-log_file   = 'BGL_2k.log'  # The input log file name
-log_format = '<Label> <Timestamp> <Date> <Node> <Time> <NodeRepeat> <Type> <Component> <Level> <Content>'  # BGL log format
-# Regular expression list for optional preprocessing (default: [])
-regex      = [r'core\.\d+',
-              r'(?<=r)\d{1,2}',
-              r'(?<=fpr)\d{1,2}',
-              r'(0x)?[0-9a-fA-F]{8}',
-              r'(?<=\.\.)0[xX][0-9a-fA-F]+',
-              r'(?<=\.\.)\d+(?!x)',
-              r'\d+(?=:)',
-              r'^\d+$',  #only numbers
-              r'(?<=\=)\d+(?!x)',
-              r'(?<=\=)0[xX][0-9a-fA-F]+',
-              r'(?<=\ )[A-Z][\+|\-](?= |$)',
-              r'(?<=:\ )[A-Z](?= |$)',
-              r'(?<=\ [A-Z]\ )[A-Z](?= |$)'
-              ]
-st         = 0.5  # Similarity threshold
-depth      = 4  # Depth of all leaf nodes
 
-parser = LogParser(log_format, indir=input_dir, outdir=output_dir,  depth=depth, st=st, rex=regex)
-parser.parse(log_file)
+def structure_bgl(input_dir, output_dir, log_file):
+    log_format = '<Label> <Timestamp> <Date> <Node> <Time> <NodeRepeat> <Type> <Component> <Level> <Content>'  # BGL log format
+    # Regular expression list for optional preprocessing (default: [])
+    regex = [r'core\.\d+',
+             r'(?<=r)\d{1,2}',
+             r'(?<=fpr)\d{1,2}',
+             r'(0x)?[0-9a-fA-F]{8}',
+             r'(?<=\.\.)0[xX][0-9a-fA-F]+',
+             r'(?<=\.\.)\d+(?!x)',
+             r'\d+(?=:)',
+             r'^\d+$',  # only numbers
+             r'(?<=\=)\d+(?!x)',
+             r'(?<=\=)0[xX][0-9a-fA-F]+',
+             r'(?<=\ )[A-Z][\+|\-](?= |$)',
+             r'(?<=:\ )[A-Z](?= |$)',
+             r'(?<=\ [A-Z]\ )[A-Z](?= |$)'
+             ]
+    st = 0.5  # Similarity threshold
+    depth = 4  # Depth of all leaf nodes
 
+    parser = LogParser(log_format, indir=input_dir, outdir=output_dir, depth=depth, st=st, rex=regex)
+    parser.parse(log_file)
+
+
+structure_bgl('../data/BGL/', '../data/BGL', 'BGL.log')
