8a514841d2d4cb2144b9bdf536fd0d57cd2ce7a8
Billy <billy.bouchard03@gmail.com>
2023-10-16 07:49:15 -0400

    add models

--- src/main.py
+++ src/main.py
@@ -4,11 +4,10 @@ from models.traditional import DecisionTree
 from models.traditional import LR
 from extensions.stat_ranking import ModelData
 import pandas as pd
+import matplotlib.pyplot as plt
+from functools import reduce
 
 
-train_x, train_y, test_x, test_y = split_bgl()
-
-model_data = ModelData(train_x, train_y, test_x, test_y)
 
 def lr_model_eval(x_train, y_train, x_test, y_test):
     lr = LR(max_iter=100000)
@@ -23,7 +22,23 @@ def decision_tree_model_eval(x_train, y_train, x_test, y_test):
 def SVM_model_eval(x_train, y_train, x_test, y_test):
     svm = SVM(train_x, train_y, test_x, test_y)
     return svm.evaluate()
-  
+
 k_fold = 10
 
-print(model_data.cross_validate(k_fold, SVM_model_eval, True))
+labels = ['LR', 'Tree', 'SVM']
+models = [lr_model_eval, decision_tree_model_eval, SVM_model_eval]
+
+data = [[]] * len(models)
+
+for i in range(100):
+    train_x, train_y, test_x, test_y = split_bgl(should_split_test_set=False)
+    model_data = ModelData(train_x, train_y, test_x, test_y)
+
+    for i in range(len(models)):
+        metric = models[i](train_x, train_y, test_x, test_y)
+        data[i].append(metric[2])
+
+
+plt.boxplot(data, patch_artist=True, labels=labels)
+
+plt.show()
